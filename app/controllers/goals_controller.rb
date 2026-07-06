eb7e362bedc11270ba1a9c98ad83bd7f85f02b43
nirow <gunmonyk@gmail.com>
2026-07-06 21:30:31 +0200

    feat: add controller actions for goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,10 +1,44 @@
 class GoalsController < ApplicationController
+  before_action :set_goal, only: [:show, :edit, :update, :destroy]
+
   def show
   end
 
   def new
+    @goal = Goal.new
   end
 
   def edit
+  end
+
+  def update
+    if @goal.update(goal_params) and @goal.habit_id == params[:habit_id].to_i
+      redirect_to habit_goal_path(@goal.habit, @goal), notice: "Goal updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    @goal.habit_id = params[:habit_id]
+
+    if @goal.save
+      redirect_to habit_goal_path(@goal.habit, @goal), notice: "Goal created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_goal
+    if @goal.habit_id == params[:habit_id].to_i
+      @goal = Goal.find(params[:id])
+    end
+  end
+
+  def goal_params
+    params.require(:goal).permit(:title, :target_value, :current_value, :start_date, :end_date)
   end
 end
--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,13 +1,49 @@
 class HabitsController < ApplicationController
+  before_action :set_habit, only: [:show, :edit, :update, :destroy]
+
   def index
+    @habits = Habit.all
   end
 
   def show
   end
 
   def new
+    @habit = Habit.new
+  end
+
+  def create
+    @habit = Habit.new(habit_params)
+    if @habit.save
+      redirect_to @habit, notice: "Habit created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @habit.update(habit_params)
+      redirect_to @habit, notice: "Habit updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @habit.destroy
+    redirect_to habits_path, notice: "Habit deleted."
+  end
+
+  private
+
+  def set_habit
+    @habit = Habit.find(params[:id])
+  end
+
+  def habit_params
+    params.require(:habit).permit(:name, :description, :frequency_type, :active)
   end
 end
